0ba5d51671925ad0783546b8790d7cf0446de708
Olivier Le Merdy <olivier.lemerdy+git@gmail.com>
2022-07-19 21:11:40 +0200

    Initial commit

---
+++ src/main/java/org/lafeuille/demo/movies/DemoSpringNeo4jMoviesApplication.java
@@ -0,0 +1,13 @@
+package org.lafeuille.demo.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringNeo4jMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringNeo4jMoviesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lafeuille/demo/movies/DemoSpringNeo4jMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lafeuille.demo.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringNeo4jMoviesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
