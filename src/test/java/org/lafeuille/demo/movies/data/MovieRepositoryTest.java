ddc72bc1e85c25bb4e763a6a334332f62077752f
Olivier Le Merdy <olivier.lemerdy+git@gmail.com>
2022-07-20 23:47:58 +0200

    Trying to run script in container

--- src/test/java/org/lafeuille/demo/movies/data/MovieRepositoryTest.java
+++ src/test/java/org/lafeuille/demo/movies/data/MovieRepositoryTest.java
@@ -1,8 +1,8 @@
 package org.lafeuille.demo.movies.data;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -11,10 +11,35 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.test.StepVerifier;
 
-class MovieRepositoryTest extends BaseRepositoryTest {
+import java.io.IOException;
+
+import static org.assertj.core.api.Assumptions.assumeThat;
+import static org.testcontainers.containers.BindMode.READ_ONLY;
+
+@SpringBootTest // @DataNeo4jTest
+@Testcontainers
+class MovieRepositoryTest {
+
+    @Container
+    private static final Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:4.4")
+            .withoutAuthentication()
+            .withClasspathResourceMapping("scripts/movies.cypher", "/tmp/movies.cypher", READ_ONLY);
 
     @Autowired
     private MovieRepository repository;
+
+    @DynamicPropertySource
+    static void neo4jProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.neo4j.uri", neo4jContainer::getBoltUrl);
+        registry.add("spring.neo4j.authentication.username", () -> "neo4j");
+        registry.add("spring.neo4j.authentication.password", neo4jContainer::getAdminPassword);
+    }
+
+    @BeforeAll
+    static void setUp() throws IOException, InterruptedException {
+        var execResult = neo4jContainer.execInContainer("./bin/cypher-shell -u neo4j -f /tmp/movies.cypher");
+        assumeThat(execResult.getExitCode()).isEqualTo(0);
+    }
 
     @Test
     void find_all() {
--- src/test/java/org/lafeuille/demo/movies/data/PersonRepositoryTest.java
+++ src/test/java/org/lafeuille/demo/movies/data/PersonRepositoryTest.java
@@ -2,12 +2,30 @@ package org.lafeuille.demo.movies.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.Neo4jContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.test.StepVerifier;
 
-class PersonRepositoryTest extends BaseRepositoryTest {
+@SpringBootTest // @DataNeo4jTest
+@Testcontainers
+class PersonRepositoryTest {
+
+    @Container
+    private static final Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:4.4");
 
     @Autowired
     private PersonRepository repository;
+
+    @DynamicPropertySource
+    static void neo4jProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.neo4j.uri", neo4jContainer::getBoltUrl);
+        registry.add("spring.neo4j.authentication.username", () -> "neo4j");
+        registry.add("spring.neo4j.authentication.password", neo4jContainer::getAdminPassword);
+    }
 
     @Test
     void find_all() {
